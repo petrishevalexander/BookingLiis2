89d728805579bc9f7d42e3b529e781f917e97dd1
Aleksandr Petrischev <sash_petrishev@mail.ru>
2022-04-08 16:43:37 +0300

    test top tab

--- src/screens/HotelScreen.js
+++ src/screens/HotelScreen.js
@@ -4,6 +4,7 @@ import {Text, View} from 'react-native';
 //
 export const HotelScreen = ({route}) => {
   const hotel = route.params.hotel;
+
   return (
     <View>
       <Text>{hotel.hotelName}</Text>
